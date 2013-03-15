e6fdbbbac40a279577940d980be098242c9234ec
henghonglee <henghong.lee@gmail.com>
2013-03-16 03:21:04 +0800

    except

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,9 +25,9 @@ class ItemsController < ApplicationController
 
   def update
     @itemToEdit = Item.find(params[:id])
-#    @itemToEdit.update_attributes(params[:item])
-    @itemToEdit.images.delete_at(params[:delete].to_i)
-    @itemToEdit.low_res_images.delete_at(params[:delete].to_i)    
+    @itemToEdit.update_attributes(params[:item])
+#    @itemToEdit.images.delete_at(params[:delete].to_i)
+#    @itemToEdit.low_res_images.delete_at(params[:delete].to_i)    
     if @itemToEdit.save
       render :json => {:status=>201 , :success=>true}
     else
