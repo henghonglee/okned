fa455e7223aa59a9ca7a12f8342ce3c75b3d5612
henghonglee <henghong.lee@gmail.com>
2013-06-13 11:01:26 +0800

    added redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :redirect_if_old
+  protected
 
+    def redirect_if_old
+      if request.host == 'tastebuds-dev.herokuapp.com'
+        redirect_to "http://54.251.168.255#{request.request_uri}", :status => :moved_permanently 
+      end
+    end
 end
